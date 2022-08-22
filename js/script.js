b82e609c257789c745e1f14ef0318aa8645af80c
Saqib Ahmad <you@example.com>
2022-08-22 17:30:06 +0600

    player Expense calculation programmed using js

--- js/script.js
+++ js/script.js
@@ -5,20 +5,36 @@ function selectPlayer(player) {
 
     const playerList = document.getElementById("player-list");
     const totalPlayers = playerList.children.length;
-    // console.log(totalPlayers)
     if (totalPlayers > 4) {
         alert ('Can Not Add More Than Five Players!!');
         return;
     }
-
+    
     playerCount++;
     const li = document.createElement('li');
     li.innerText = `${playerCount + '. ' + playerName}`
-
+    
     playerList.appendChild(li);
-
+    
     document.getElementById('nubmer-of-selected-players').innerText = playerList.children.length;
     
     player.style.backgroundColor = "gray";
     player.setAttribute('disabled', true);
 }
+
+
+// Geting input value by a function
+function getInputValueById(inputId){
+    const inputfield = document.getElementById(inputId).value;
+    const inputValue = parseFloat(inputfield);
+    return inputValue;
+}
+
+
+// Event listner to calculate Button
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const perPlayerCost = getInputValueById("per-player-cost");
+    document.getElementById('player-expense').innerText = perPlayerCost * playerCount;
+    // console.log(playerCount);
+
+})
