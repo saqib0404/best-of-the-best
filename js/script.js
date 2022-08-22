c5eabe6c9d14f92cbc5a6f6a43a139ca6f2b8b51
Saqib Ahmad <you@example.com>
2022-08-22 16:32:22 +0600

    Player Selection are added dynamically using js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-
 // Player Selection
-function selectPlayer(player){
-    console.log(player);
+let playerCount = 0;
+function selectPlayer(player) {
+    const playerName = player.parentNode.children[0].innerText;
+
+    const playerList = document.getElementById("player-list");
+    const totalPlayers = playerList.children.length;
+    // console.log(totalPlayers)
+    if (totalPlayers > 4) {
+        alert ('Can Not Add More Than Five Players!!');
+        return;
+    }
+
+    playerCount++;
+    const li = document.createElement('li');
+    li.innerText = `${playerCount + '. ' + playerName}`
+
+    playerList.appendChild(li);
+
+    document.getElementById('nubmer-of-selected-players').innerText = playerList.children.length;
+    
+    player.style.backgroundColor = "gray";
+    player.setAttribute('disabled', true);
 }
